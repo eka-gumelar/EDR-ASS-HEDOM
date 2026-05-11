697d25d16e7d5d3bd1fbf3795be5e2c0c86175fb
eka-gumelar <miracle.khae@gmail.com>
2026-05-11 11:34:30 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -21,7 +21,7 @@
             init: async function() {
                 try {
                     // 1. Ubah nama ini menjadi myRealFirebaseConfig
-                    const firebaseConfig = {
+                    const myRealFirebaseConfig = {
                           apiKey: "AIzaSyCB7kD9O6JCLxI_mDN7v5XhDXOYZFCxroI",
                           authDomain: "edr-assembling-hedom.firebaseapp.com",
                           databaseURL: "https://edr-assembling-hedom-default-rtdb.asia-southeast1.firebasedatabase.app",
