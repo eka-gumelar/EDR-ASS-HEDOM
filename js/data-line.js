9c301e6d7da6d70206030f98b4b43a0800e49bbc
eka-gumelar <miracle.khae@gmail.com>
2026-05-11 13:47:52 +0700

    Update data-line.js

--- js/data-line.js
+++ js/data-line.js
@@ -1,7 +1,3 @@
     let initMasterDataLine = [
         { id: "Line 1", target: 28000, last_edited: "2024-05-01" },
-        { id: "Line 2", target: 26000, last_edited: "2024-05-01" },
-        { id: "Line 3", target: 16000, last_edited: "2024-05-01" },
-        { id: "Line 4", target: 10000, last_edited: "2024-05-01" },
-        { id: "Line 5", target: 2000, last_edited: "2024-05-01" }
-    ];
+     ];
